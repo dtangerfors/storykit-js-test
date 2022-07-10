69bae3f79a666c693f632f172abb93c353e99427
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-07-10 21:09:01 +0200

    Moved interface to own file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-interface Video {
-  id: number;
-  title: string;
-  grade: number;
-}
+import axios from "axios";
+import renderVideos from "./renderListElements"
+import { Video } from "./model";
 
-// GET http://localhost:3000/videos
-const getVideos = (): Promise<Video[]> => {
-}
+const form = document.querySelector(".js-form") as HTMLFormElement;
+const searchInput = document.querySelector(".js-input") as HTMLInputElement;
+const resultList = document.querySelector(".js-result") as HTMLUListElement;
 
-// PUT http://localhost:3000/videos/${id}
-const updateVideo = async (id, changes): Promise<Video> => {};
+// GET videos on search
+const queryResult = async (query: String) => {
+  let fetchURL = "http://localhost:3000/videos/";
+  fetchURL += `?q=${query}`;
+  if (query) {
+    axios.get(fetchURL).then((res) => {
+      const foundVideos: Video[] = res.data;
+      renderVideos(foundVideos, resultList);
+    });
+    resultList.classList.add("has-results");
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  queryResult(searchInput.value);
+});
